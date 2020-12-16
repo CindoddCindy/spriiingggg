d7b2843c88f5f7d601df10248138dcd0cccebbef
CindoddCindy <cindodcindy@gmail.com>
2020-12-16 19:17:48 +0700

    tambah kelas kelas

---
+++ myspringspringtest/src/main/java/springspring/myspringspringtest/MyspringspringtestApplication.java
@@ -0,0 +1,13 @@
+package springspring.myspringspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringspringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringspringtestApplication.class, args);
+	}
+
+}
---
+++ myspringspringtest/src/test/java/springspring/myspringspringtest/MyspringspringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package springspring.myspringspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringspringtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
