9c77987991534034021730679f6de3fca6aa6295
CindoddCindy <cindodcindy@gmail.com>
2020-12-16 20:18:19 +0700

    tambah kelas kelas lagi

--- myspringspringtest/src/main/java/springspring/myspringspringtest/repository/RoleRepository.java
+++ myspringspringtest/src/main/java/springspring/myspringspringtest/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package springspring.myspringspringtest.repository;
 
-public interface RoleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springspring.myspringspringtest.models.ERole;
+import springspring.myspringspringtest.models.Role;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(ERole name);
 }
--- myspringspringtest/src/main/java/springspring/myspringspringtest/repository/UserRepository.java
+++ myspringspringtest/src/main/java/springspring/myspringspringtest/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package springspring.myspringspringtest.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springspring.myspringspringtest.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
